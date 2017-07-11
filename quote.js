0cdb2e3170daffc12ae5e9c357cad05375e4c9c5
MadiBentley <mb.madisonbentley@gmail.com>
2017-07-11 07:33:01 -0700

    Make function work with calling quote

--- quote.js
+++ quote.js
@@ -1,12 +1,13 @@
-$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-  $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-});
-function generateQuote() = {
 
-}
+//function generateQuote() = {
+  //$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    //$("#quoteSpace").append(a[0].content + "<p>— " + a[0].title + "</p>")
+  //});
+//}
 
-function displayQuote() = {
-  $(document).ready(function(){
-    ("#quoteSpace").empty().append(generateQuote());
-  });
+function displayQuote() {
+  console.log("called");
+  //$(document).ready(function(){
+    //generateQuote();
+  //});
 }
