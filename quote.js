bfb6983403066b2013cd01a04e6aaf8d30f384e5
MadiBentley <mb.madisonbentley@gmail.com>
2017-07-11 07:42:29 -0700

    add favourites fn and button

--- quote.js
+++ quote.js
@@ -1,13 +1,18 @@
+var quote = "";
 
 function generateQuote() {
   $(document).ready(function(){
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $("#quoteSpace").html(a[0].content + "<p>— " + a[0].title + "</p>")
+      $("#quoteSpace").html(a[0].content + "<p>— " + a[0].title + "</p>");
+      quote = a[0].content + "<p>-" + a[0].title + "</p>";
     });
   });
 }
 
 function displayQuote() {
-  console.log("called");
     generateQuote();
+}
+
+function addToFavourites(){
+  $("#favQuotes").append(quote);
 }
