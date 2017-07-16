0138c7699d5ac0ad9d5e247cfe37569f7fc486f3
MadiBentley <mb.madisonbentley@gmail.com>
2017-07-16 00:03:20 -0700

    change border box to padding box

--- quote.js
+++ quote.js
@@ -11,6 +11,7 @@ function generateQuote() {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
       $("#quoteSpace").html(a[0].content + "<p>\— " + a[0].title + "</p>");
       quote = a[0].content + "<p>-" + a[0].title + "</p>";
+      console.log(a[0].content);
     });
   });
 }
