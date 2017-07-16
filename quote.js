73afe9f7d638cdebf47cf06f804a45b0db6d47cf
MadiBentley <mb.madisonbentley@gmail.com>
2017-07-15 22:59:11 -0700

    add charset utf8 to hed

--- quote.js
+++ quote.js
@@ -9,7 +9,7 @@ function init(){
 function generateQuote() {
   $(document).ready(function(){
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $("#quoteSpace").html(a[0].content + "<p>— " + a[0].title + "</p>");
+      $("#quoteSpace").html(a[0].content + "<p>\— " + a[0].title + "</p>");
       quote = a[0].content + "<p>-" + a[0].title + "</p>";
     });
   });
