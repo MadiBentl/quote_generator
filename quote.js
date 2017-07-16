ddfb9ebb51a83e2f882a979bcc7ae575be614ef1
MadiBentley <mb.madisonbentley@gmail.com>
2017-07-16 00:32:09 -0700

    add graduated transparency to favourite quotes text

--- quote.js
+++ quote.js
@@ -11,7 +11,6 @@ function generateQuote() {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
       $("#quoteSpace").html(a[0].content + "<p>\— " + a[0].title + "</p>");
       quote = a[0].content + "<p>-" + a[0].title + "</p>";
-      quoteToCopy = a[0].content;
       console.log(a[0].content);
     });
   });
@@ -24,7 +23,7 @@ function displayQuote() {
 
 function addToFavourites(){
   if (favQuotesArr[0] != quote){
-    $("#favQuotes").append(quoteToCopy);
+    $("#favQuotes").prepend(quote);
     favQuotesArr.unshift(quote);
   }
 }
